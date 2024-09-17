83f367785f868016239acc0701fe74ec7dceca04
Yace Sf <yacine.sf@outlook.fr>
2024-09-17 14:27:51 +0200

    Changement de header

--- src/pages/header/index.js
+++ src/pages/header/index.js
@@ -3,21 +3,28 @@ import "./index.css";
 function Header() {
   return (
     <div className="header">
-      <h1 id="page-title">Y.SAFSAFI</h1>
+      <h1 id="page-title">Y.SF Portfolio</h1>
       <nav>
         <ul className="header__ul">
           <li className="header__ul__li">
-          <a href="#">PRESENTATION</a>
+            <a href="#">PRESENTATION</a>
           </li>
           <li className="header__ul__li">
-          <a href="#">PROJET</a>
+            <a href="#">PROJET</a>
           </li>
           <li className="header__ul__li">
-          <a href="#">CONTACT</a>
+            <a href="#">CONTACT</a>
           </li>
         </ul>
       </nav>
-        <div className="header__cv">MON CV</div>
+      <div className="header__link">
+        <a href="#" className="header__link__item">
+          <i className="fa-brands fa-linkedin"></i>
+        </a>
+        <a href="#" className="header__link__item">
+          MON CV
+        </a>
+      </div>
     </div>
   );
 }
